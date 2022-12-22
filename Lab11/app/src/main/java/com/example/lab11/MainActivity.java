b614ca9749dcb680365117fba71521b9a82222ac
Stella17965 <111170156+Stella17965@users.noreply.github.com>
2022-12-22 20:57:09 +0800

    coding at home

--- Lab11/app/src/main/java/com/example/lab11/MainActivity.java
+++ Lab11/app/src/main/java/com/example/lab11/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -40,9 +41,10 @@ public class MainActivity extends AppCompatActivity {
         btn_update=findViewById(R.id.btn_update);
         listView=findViewById(R.id.listView);
 
-        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items;
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
         listView.setAdapter(adapter);
         dbrw=new MyDBHelper(this).getWritableDatabase();
+
         btn_insert.setOnClickListener(view -> {
             if(ed_book.length()<1 || ed_price.length()<1){
                 Toast.makeText(MainActivity.this,"欄位請勿留空",Toast.LENGTH_SHORT).show();
@@ -57,6 +59,54 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this,"新增失敗"+e.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
+        });
+        btn_update.setOnClickListener(view -> {
+            if(ed_book.length() <1 || ed_price.length()<1){
+                Toast.makeText(MainActivity.this,"欄位請勿留空",Toast.LENGTH_SHORT).show();
+            }else{
+                try{
+                    dbrw.execSQL("INSERT INTO myTable(book,price) values(?,?)",new Object[]{ed_book.getText().toString(),ed_price.getText().toString()});
+                    Toast.makeText(MainActivity.this,"成功書名"+ed_book.getText().toString()+" 價格"+ed_price.getText().toString(),Toast.LENGTH_SHORT).show();
+                    ed_book.setText("");
+                    ed_price.setText("");
+                }catch(Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"新增失敗:"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btn_delete.setOnClickListener(view -> {
+            if(ed_book.length()<1){
+                Toast.makeText(MainActivity.this,"書名請勿留空",Toast.LENGTH_SHORT).show();
+            }else{
+                try{
+                    dbrw.execSQL("DELETE FROM myTable WHERE book LIKE'"+ed_book.getText().toString()+"'");
+                    Toast.makeText(MainActivity.this,"刪除書名"+ed_book.getText().toString(),Toast.LENGTH_SHORT).show();
+                    ed_book.setText("");
+                    ed_price.setText("");
+                }catch(Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"刪除失敗"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        btn_query.setOnClickListener(view -> {
+            Cursor c;
+            if(ed_book.length()<1){
+                c=dbrw.rawQuery("SELECT * FROM myTable",null);
+            }else{
+                c=dbrw.rawQuery("SELECT * FROM myTable WHERE book LIKE '"+ed_book.getText().toString()+"'",null);
+            }
+            c.moveToFirst();
+            items.clear();
+            Toast.makeText(MainActivity.this,"共有"+c.getCount()+"筆",Toast.LENGTH_SHORT).show();
+            for(int i=0;i<c.getCount();i++){
+                items.add("書籍:"+c.getString(0)+"\t\t\t\t價格:"+c.getString(1))   ;
+                c.moveToNext();
+            }
+            adapter.notifyDataSetChanged();
+            c.close();
         });
     }
 }
